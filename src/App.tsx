59c6529db5ec7aa9ff419ec0c3b33d07b1b3b0f6
sunny <284659536+sunnyyaoops@users.noreply.github.com>
2026-05-15 11:13:48 +0800

    Add base url for router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,38 @@
 import { Arrival } from "./arrival/Arrival";
 import { OCEAN_SUNSET_LOCATION_ID } from "./departure/helpers/getOceanSunsetLocation";
 import { Departure } from "./departure/Departure";
-import { Route, Switch } from "wouter";
+import { Route, Router, Switch } from "wouter";
 import { Intro } from "./Intro";
 import { RedirectionPage } from "./RedirectionPage";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route path="/" component={Intro} />
-      <Route path="/departure" component={Departure} />
-      <Route path="/arrival/:cityId">
-        {(params) => <Arrival cityId={params.cityId} sunsetOcean={null} />}
-      </Route>
-      <Route path="/arrival/ocean/:nameAscii/:lat/:lng">
-        {(params) => (
-          <Arrival
-            cityId={null}
-            sunsetOcean={{
-              id: OCEAN_SUNSET_LOCATION_ID,
-              name: params.nameAscii,
-              nameAscii: params.nameAscii,
-              lat: parseFloat(params.lat),
-              lng: parseFloat(params.lng),
-            }}
-          />
-        )}
-      </Route>
-      <Route>
-        <RedirectionPage />
-      </Route>
-    </Switch>
+    <Router base={import.meta.env.BASE_URL.replace(/\/$/, "")}>
+      <Switch>
+        <Route path="/" component={Intro} />
+        <Route path="/departure" component={Departure} />
+        <Route path="/arrival/:cityId">
+          {(params) => <Arrival cityId={params.cityId} sunsetOcean={null} />}
+        </Route>
+        <Route path="/arrival/ocean/:nameAscii/:lat/:lng">
+          {(params) => (
+            <Arrival
+              cityId={null}
+              sunsetOcean={{
+                id: OCEAN_SUNSET_LOCATION_ID,
+                name: params.nameAscii,
+                nameAscii: params.nameAscii,
+                lat: parseFloat(params.lat),
+                lng: parseFloat(params.lng),
+              }}
+            />
+          )}
+        </Route>
+        <Route>
+          <RedirectionPage />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
